46f2ffe65c1944fbfbdc495582c687bfbe26192f
Gautam-star519 <gopalp@alankit.com>
2026-05-12 13:18:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,6 @@ def webhook():
     return 'EVENT_RECEIVED', 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
